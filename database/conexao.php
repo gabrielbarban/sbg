ab913ff292cd9e1bf988873806fa0f346c147c5b
root <root@instance-1.us-east1-b.c.woven-woodland-231512.internal>
2019-07-31 18:35:38 +0000

    Inclusao do database

---
+++ database/conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+class Conexao
+{
+        public $host='localhost';
+        public $port='3306';
+        public $user='gabriel';
+        public $password='gabriel';
+        public $dbname="controle";
+}
+
+?>
